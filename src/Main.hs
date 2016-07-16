0668481aea2dc9fe7e6657429965ebc46676be21
Regis <boudinot.regis@yahoo.com>
2016-07-15 18:30:36 -0600

    formatting

--- src/Main.hs
+++ src/Main.hs
@@ -6,9 +6,11 @@ add x y z  =  x + y + z
 main = do
   let collection = [0, 1, 2, 3]
   let added = add 2 3 8
+
   -- printing regular stuff
   print "Hello, Haskell!"
   print "WOW"
+
   -- printing a collection and a function result
   print collection
   print added
