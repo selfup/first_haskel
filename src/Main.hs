43dec81ddf43a9e3c970af9339235930cd213aa0
Regis <boudinot.regis@yahoo.com>
2016-07-15 18:13:39 -0600

    lil update

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,11 @@
 module Main where
 
-add :: Integer -> Integer -> Integer
-add x y  =  x + y
+add :: Integer -> Integer -> Integer -> Integer
+add x y z  =  x + y + z
 
 main = do
   let array_list_vec = [0, 1, 2, 3]
-  let added = add 2 3
+  let added = add 2 3 8
   -- printing regular stuff
   print "Hello, Haskell!"
   print "WOW"
