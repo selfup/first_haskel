60e24aef05f4ab086ef76307c8268a56bb64e7dc
Regis <boudinot.regis@yahoo.com>
2016-07-15 18:24:52 -0600

    okk

--- src/Main.hs
+++ src/Main.hs
@@ -4,11 +4,11 @@ add :: Integer -> Integer -> Integer -> Integer
 add x y z  =  x + y + z
 
 main = do
-  let array_list_vec = [0, 1, 2, 3]
+  let collection = [0, 1, 2, 3]
   let added = add 2 3 8
   -- printing regular stuff
   print "Hello, Haskell!"
   print "WOW"
   -- printing a collection and a function result
-  print array_list_vec
+  print collection
   print added
